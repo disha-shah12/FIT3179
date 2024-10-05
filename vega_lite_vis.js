2c7d89079a9ca138e05c1ac417290784af550937
disha-shah12 <dsha0044@student.monash.edu>
2024-10-05 20:58:50 +1000

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,5 @@
+var vg_1 = "area_burn_map.vg.json";
+vegaEmbed("#area_burn_map", vg_1).then(function(result) {
+    // Access the Vega view instance
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
