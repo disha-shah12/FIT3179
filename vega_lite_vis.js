506927620743c53babdede2d3fa95f6fe77991f2
disha-shah12 <dsha0044@student.monash.edu>
2024-10-06 21:32:17 +1100

    adding weather visualisation

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,3 +3,9 @@ vegaEmbed("#area_burn_map", vg_1).then(function(result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "weather_chart.vg.json";
+vegaEmbed("#weather_chart", vg_2).then(function(result) {
+    // Access the Vega view instance
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
