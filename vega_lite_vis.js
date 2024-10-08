84c87c2e749a4976c6a96f0e045103eddd5461c2
disha-shah12 <dsha0044@student.monash.edu>
2024-10-08 13:14:55 +1100

    adding temp_change graph

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -9,3 +9,9 @@ vegaEmbed("#weather_chart", vg_2).then(function(result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "temp_change.vg.json";
+vegaEmbed("#temp_change", vg_3).then(function(result) {
+    // Access the Vega view instance
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
