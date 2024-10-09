5234a23717206be8fcde8bdf1e034b4045b4975e
disha-shah12 <dsha0044@student.monash.edu>
2024-10-09 23:28:21 +1100

    adding crude_rate vis

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -4,12 +4,6 @@ vegaEmbed("#area_burn_map", vg_1).then(function(result) {
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "weather_chart.vg.json";
-vegaEmbed("#weather_chart", vg_2).then(function(result) {
-    // Access the Vega view instance
-    //(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-
 var vg_3 = "temp_change.vg.json";
 vegaEmbed("#temp_change", vg_3).then(function(result) {
     // Access the Vega view instance
@@ -18,6 +12,12 @@ vegaEmbed("#temp_change", vg_3).then(function(result) {
 
 var vg_4 = "avg_rainfall.vg.json";
 vegaEmbed("#avg_rainfall", vg_4).then(function(result) {
+    // Access the Vega view instance
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "crude_rate.vg.json";
+vegaEmbed("#crude_rate", vg_5).then(function(result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
